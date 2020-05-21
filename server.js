f01f5caf0f5caeed6ba6ee8e682146419f556837
Grasscopper <jssstransky@gmail.com>
2020-05-21 19:28:34 -0400

    Initial Commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const MongoClient = require('mongodb').MongoClient
+const express = require('express');
+const app = express();
+const port = 5000;
+
+require('dotenv').config()
+let connectionString = process.env.REACT_APP_KEY
+
+MongoClient.connect(connectionString, { useUnifiedTopology: true }, (err, client) => {
+  if (err) return console.error(err)
+  console.log('Connected to Database')
+  const db = client.db('character-creation')
+  const characters = db.collection('characters')
+
+  app.get('/characters', (req, res) => {
+    characters.distinct("name")
+    .then((body) => {
+      res.send(body)
+    })
+  });
+
+  app.listen(port, () => console.log(`Listening on port ${port}`));
+})
